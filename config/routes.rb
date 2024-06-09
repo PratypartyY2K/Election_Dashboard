806e9f00b70bb4035d7f9b9c4ed1130d9969f33e
pratkumar <pratkumar@enphaseenergy.com>
2024-06-09 11:44:52 +0530

    Add route for application health-check in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,26 @@
 Rails.application.routes.draw do
+
+  get 'up' => "rails/health#show", as: :rails_health_check
+
   # Define the root route if you have a home page
   root 'pages#home'
+  
   get 'users/gender_options', to: 'users#gender_options'
   get 'candidates/gender_options', to: 'candidates#gender_options'
   get 'constituencies/type_options', to: 'constituencies#type_options'
   # Resources routes
   resources :users
+  
   resources :candidates
+  
   resources :constituencies do
     resources :users
   end
+  
   resources :parties do
     resources :candidates
   end
+  
   resources :constituencies do
     resources :candidates
   end
